7d3b48218bbe4e8b1d5e98f0062a573f27c83d4b
Bojana Skarich <bojana.skarich@gmail.com>
2015-03-12 21:52:08 -0700

    Modify home route to include Twig template rendering. Create a create_contact.twig file and a delete_contacts.twig file.

--- app/app.php
+++ app/app.php
@@ -2,6 +2,13 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Contact.php";
 
+//Store the objects in cookies in the user's browser
+
+    session_start();
+
+    if (empty($_SESSION['list_of_contacts']))
+        $_SESSION['list_of_contacts'] = array();
+
     $app = new Silex\Application();
     $app['debug'] = true;
 
@@ -9,84 +16,38 @@
     'twig.path' => __DIR__.'/../views'
 	));
 
-    //Store the objects in cookies in the user's browser
-    session_start();
-    if (empty($_SESSION['$list_of_contacts']))
-    	$_SESSION['$list_of_contacts'] = array();
-
  
+ //Home route
 
     $app->get("/", function() use ($app) {
     
-        $output = "";
-
-        $list_of_contacts = Contact::getAll();
-
-            if(!empty($list_of_contacts)) {
-                $output = $output . "
-                <h1>List of Contacts</h1>
-                <p>Here are your contacts:</p>
-                <ul>";
-
-                foreach ($list_of_contacts as $contact) {
-                    $output .= "<p>" . $contact->getName() . "</p>";
-                    $output .= "<p>" . $contact->getPhone_Number() . "</p>";
-                    $output .= "<p>" . $contact->getAddress() . "</p>";
-                }
-
-                $output .= "</ul>";
-
-            }
-
-            $output .= "
-                <form action='/contacts' method='post'>
-                    <label for='name'>Name</label>
-                    <input id='name' name='name' type='text'>
-
-                    <button type='submit'>Add task</button>
-                </form>
-        ";
-
-            $output .= "
-                <form action='/delete_contacts' method='post'>
-                    <button type='submit'>delete</button>
-                </form>
-";
-
         return $app['twig']->render('contacts.twig', array('contacts' => Contact::getAll()));
     });
 
-    //This route displays contact via the post method
 
-    $app->post("/view_contacts", function() {
+//View_Contacts page route
+
+    $app->post("/view_contacts", function() use ($app) {
 
     	$contact = new Contact($_POST['name']);
+        $contact = new Contact($_POST['phone_number']);
+        $contact = new Contact($_POST['address']);
     	$contact->save();
-    	return "
 
-    	<h1>You created a new contact!</h1>
-    	<p>" . $contact->getName() . "</p>
-    	<p>" . $contact->getPhone_Number() . "</p>
-    	<p>" . $contact->getAddress() . "</p>
-    	<p><a href='/'>View your list of contacts</a></p>
-
-    	";
+    	return $app['twig']->render('create_contact.twig', array('newcontact' => $contact));
 
     });
 
-    //Deletes contacts display text
 
-    $app->post("/delete_contacts", function() {
+//Delete_Contacts page route
+
+    $app->post("/delete_contacts", function() use ($app) {
 
     	Task::deleteAll();
 
-    	return "
-        <h1>Address book cleared!</h1>
-        <p><a href='/''> Back to Home</a></p>
-    ";
+    	return $app['twig']->render('delete_contacts.twig');
 
     });
-
 
     return $app;
 ?>
