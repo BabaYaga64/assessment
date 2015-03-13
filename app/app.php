881925f1beff29d2a0504e636b694b0297cf8f97
Bojana Skarich <bojana.skarich@gmail.com>
2015-03-12 20:27:38 -0700

    Modify first home page route in app.php. Change class declaration file name to Contact.php.

--- app/app.php
+++ app/app.php
@@ -1,8 +1,9 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/addressbook.php";
+    require_once __DIR__."/../src/Contact.php";
 
     $app = new Silex\Application();
+    $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
@@ -13,48 +14,15 @@
     	$_SESSION['$list_of_contacts'] = array();
 
 
+    //This route gets displays home page 
+
     $app->get("/", function() use ($app) {
-
-       	//This code displays a list of Contact objects already in the array
-
-        $name1 = new Contact("Virginia Woolf");
-        $phone_number1 = new Contact("503-777-8877");
-        $address1 = new Contact("8797 NW 26th Ave, Portland OR 97392");j    
-
-        $name2 = new Contact("Oscar Wilde");
-        $phone_number2 = new Contact("971-273-9874");
-        $address2 = new Contact("31231 Rockwood Drive, Lake Oswego, OR 97207");
-
-        $name3 = new Contact("Margaret Atwood");
-        $phone_number3 = new Contact("541-887-2334");
-        $address3 = new Contact("997 Elmwood Drive, Corvallis, OR 97233");
-
-        $list_of_contacts = array($name1, $phone_number1, $address1, $name2, $phone_number2, $address2, $name3, $phone_number3, $address3);
-
-        foreach (Contact::getAll() as $contact) {
-        	$output = $output . "<p>" . $contact->getName() . "</p>";
-        	$output = $output . "<p>" . $contact->getPhone_Number() . "</p>";
-        	$output = $output . "<p>" . $contact->getAddress() . "</p>";
-        }
-    
-    if (!empty($list_of_contacts)) {
-    	$output = $output . "
-    	<h1>List of Contacts</h1>
-    	<p>Here are all your contacts</p>
-    	</ul>";
-
-    	foreach ($list_of_contacts as $contact) {
-    		$output = $output . "<p>" . $contact->getName() . "</p>";
-        	$output = $output . "<p>" . $contact->getPhone_Number() . "</p>";
-        	$output = $output . "<p>" . $contact->getAddress() . "</p>";
-    	}
-
-    	$output = $output . "</ul>";
-    
 
         return $app['twig']->render('contacts.twig', array('list_of_contacts' => Contact::getAll()));
 
     });
+
+    //This route gets form input at create_contact
 
     $app->get("/create_contact", function() {
 
@@ -71,7 +39,7 @@
         return $output;
     });
 
-    //Displays new contact via post method
+    //This route displays contact via the post method
 
     $app->post("/create_contact", function() {
 
@@ -105,3 +73,4 @@
 
     return $app;
 ?>
+
---
+++ src/Contact.php
@@ -0,0 +1,79 @@
+<?php
+
+//Class declaration
+
+	class Contact 
+	{
+		private $name;
+		private $phone_number;
+		private $address;
+	}
+
+//Constructor
+
+	function __construct($name, $phone_number, $address)
+	{
+		$this->name = $name;
+		$this->phone_number = $phone_number;
+		$this->address = $address;
+	}
+
+
+//Getters n' setters
+
+	function setName($new_name)
+	{
+
+		$this->name = $new_name;	
+	}
+
+	function getName()
+	{
+
+		return $this->name;
+	}
+
+	function setPhone_Number($new_phone_number)
+	{
+
+		$this->phone_number = $new_phone_number;
+	}
+
+	function getPhone_Number()
+	{
+
+		return $this->phone_number;
+	}
+
+	function setAddress($new_address)
+	{
+		$this->address = $new_address;
+
+	}
+
+	function getAddress()
+	{
+		return $this->address;
+
+	}
+
+//Save function
+
+	function save()
+	{
+		array_push($_SESSION['list_of_contacts'], $this);
+	}
+
+//Static functions
+
+	static function getAll()
+	{
+    return $_SESSION['list_of_contacts'];
+	}
+
+	static function deleteAll()
+	{
+	return	$_SESSION['list_of_contacts'] = array();
+
+	}
+?>
